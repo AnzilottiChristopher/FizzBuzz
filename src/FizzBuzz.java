158cd6fb5a88aa92a59c760c8c23e475dcfce77f
Chris Anzilotti <anzilottichr@gmail.com>
2023-03-21 19:29:51 -0400

    Finished FizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,18 +4,23 @@ public class FizzBuzz
     {
         for(int count = 1; count <= 100; count ++)
         {
+            
             if (count % 3 == 0) 
             {
                 if (count % 5 == 0) 
                 {
                     System.out.println("FizzBuzz");
-                    break;
+                    continue;
                 }
                 System.out.println("Fizz");
             }
             else if(count % 5 == 0)
             {
                 System.out.println("Buzz");
+            }
+            else
+            {
+                System.out.println(count);
             }
         }
     }
