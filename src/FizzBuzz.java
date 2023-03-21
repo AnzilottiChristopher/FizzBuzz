b119998dca716c765f87e1549c47958adbb8cd9f
Chris Anzilotti <anzilottichr@gmail.com>
2023-03-21 19:22:19 -0400

    Added FizzBuzz Code

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,5 +2,21 @@ public class FizzBuzz
 {
     public static void main(String[] args) throws Exception 
     {
+        for(int count = 1; count <= 100; count ++)
+        {
+            if (count % 3 == 0) 
+            {
+                if (count % 5 == 0) 
+                {
+                    System.out.println("FizzBuzz");
+                    break;
+                }
+                System.out.println("Fizz");
+            }
+            else if(count % 5 == 0)
+            {
+                System.out.println("Buzz");
+            }
+        }
     }
 }
